a8174687fe4227cefe8a1c64ee4a23d5330f77bb
Prokudin Vlad <liberalvlad@gmail.com>
2022-11-07 15:51:18 +0700

    add new modules

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,6 +3,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
 import {AppLink} from "shared/ui/AppLink";
 import {AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 
 
 interface NavbarProps {
@@ -12,6 +13,7 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = ({className}) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher/>
             <div className={cls.links}>
                 <AppLink
                     theme={AppLinkTheme.SECONDARY}
