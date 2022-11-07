ae8b0c2fd46c944add62b9084194a8f68c6d6879
Prokudin Vlad <liberalvlad@gmail.com>
2022-11-07 17:47:36 +0700

    delete module ThemeSwitcher

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,7 +3,6 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
 import {AppLink} from "shared/ui/AppLink";
 import {AppLinkTheme} from "shared/ui/AppLink/AppLink";
-import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 
 
 interface NavbarProps {
@@ -13,7 +12,6 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = ({className}) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <ThemeSwitcher/>
             <div className={cls.links}>
                 <AppLink
                     theme={AppLinkTheme.SECONDARY}
